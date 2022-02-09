5424c2e31a0de64e88be6a8a3a3da134512832f1
Mohammad Shakeeb <mohammadshakeeb@cedcoss.com>
2022-02-09 12:18:35 +0530

    basic functionality

--- src/script.js
+++ src/script.js
@@ -1,3 +1,8 @@
+        $("#cancel_del").hide();
+        $(".success").hide();
+        $("#delete_product").hide();
+        $(".error").hide();
+        $("#update_product").hide();
 const arrProduct = [];
 $("#add_product").click(function(){
 var sku=$("#product_sku").val();
@@ -6,12 +11,12 @@ var price=$("#product_price").val();
 var quantity=$("#product_quantity").val();
 console.log(sku,nname,price,quantity);
 arrProduct.push({ sku: sku, name: nname, price: price,quantity:quantity });
-    addElement(arrProduct);
+    display(arrProduct);
 });
 
 
 
-    function addElement(arr) {
+    function display (arr) {
         var table =
           "<table> <tr><th>SKU</th><th>Name</th><th> Price</th><th>Quantity</th><th>Action</th></tr> ";
         for (let i = 0; i < arr.length; i++) {
@@ -26,33 +31,79 @@ arrProduct.push({ sku: sku, name: nname, price: price,quantity:quantity });
             "$" +
             arr[i].price +
             "</td><td>"+ arr[i].quantity + "</td><td>" +
-            '<a href="#" onclick=EditForm('+arr[i].sku+')>Edit</a>' +
+            '<a href="#" onclick=setElements('+arr[i].sku+')>Edit</a>' +
+            '<a href="#" onclick=deleteElements('+arr[i].sku+')>  Delete</a>'+
             "</td></tr>";
         }
         document.getElementById("table").innerHTML = table + "</table>";
+        $(".success").show();
+        $(".success").text("Product Added Successfully")
+        $(".error").hide();
+        clearInput();
       }
      
-      function EditForm(sku){
-          $("#add_product").val("Update Product");
-          $("#product_sku").val("");
-          $("#product_name").val("");
-          $("#product_price").val("");
-          $("#product_quantity").val("");
-          
+      
+        function setElements(id) {
+          $(".success").hide();
+          $("#update_product").show();
+          $("#add_product").hide();
+          var index = arrProduct.findIndex((x) => x.sku == id); //finding index of element
+          console.log(arrProduct[index]);
+          $("#product_sku").val(arrProduct[index].sku);
+          $("#product_name").val(arrProduct[index].name);
+          $("#product_price").val(arrProduct[index].price);
+          $("#product_quantity").val(arrProduct[index].quantity);
+          $("#product_sku").attr("disabled", "disabled");
       }
-      $("add_product").click(function(){
+          
+      
+      $("#update_product").click(function(){
         var sku=$("#product_sku").val();
         var nname=$("#product_name").val();
         var price=$("#product_price").val();
         var quantity=$("#product_quantity").val();
-        for (let i = 0; i < arr.length; i++) {
-            if(sku == arrProduct.sku){
-                arrProduct.sku=sku;
-                arrProduct.name=nname;
-                arrProduct.price=price;
-                arrProduct.quantity=quantity;
+        for (let i = 0; i < arrProduct.length; i++) {
+            if(sku == arrProduct[i].sku){
+                arrProduct[i].sku=sku;
+                arrProduct[i].name=nname;
+                arrProduct[i].price=price;
+                arrProduct[i].quantity=quantity;
             }
         }
-        addElement(arrProduct);
+        display(arrProduct);
+        clearInput();
+        $("#add_product").show();
+        $("#update_product").hide();
+        $("#product_sku").attr("disabled", false);
+        $(".success").show();
+        $(".error").hide();
+        $(".success").html("Product updated Successfully")
+    
         
       });
+      function clearInput(){
+        $("#product_sku").val("");
+        $("#product_name").val("");
+        $("#product_price").val("");
+        $("#product_quantity").val("");
+      }
+
+      function deleteElements(id){
+            $("#delete_product").show();
+            $("#cancel_del").show();
+            $("#delete_product").click(function(){
+              var index = arrProduct.findIndex((x) => x.sku == id);
+              arrProduct.splice(index, 1);
+              display(arrProduct);
+              $(".success").text("Product Deleted Successfully");
+              $(".success").show();
+              $("#delete_product").hide();
+              $("#cancel_del").hide();
+            });
+              // successMessage("Product deleted Successfully");
+            $("#cancel_del").click(function(){
+                $("#delete_product").hide();
+                $("#cancel_del").hide();
+            });
+            
+      }
